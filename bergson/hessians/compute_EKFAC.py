495cc85065bba4b5f6d84d84d03c38f1ad3bcb95
LouisYRYJ <louis.yousif@yahoo.de>
2025-06-24 15:32:08 +0000

    pipeline with new set up running

--- bergson/hessians/compute_EKFAC.py
+++ bergson/hessians/compute_EKFAC.py
@@ -16,7 +16,6 @@ from bergson.hessians.covariance_all_factors import (
     compute_eigendecomposition,
     compute_eigenvalue_correction,
 )
-from bergson.processing import fit_normalizers
 from bergson.utils import assert_type, get_layer_list
 
 
@@ -114,29 +113,7 @@ def worker_ekfac(rank: int, world_size: int, cfg: IndexConfig, ds: Dataset | Ite
             map_location=f"cuda:{rank}",
         )
     else:
-        if cfg.normalizer != "none":
-            # Evenly sample `stats_sample_size` examples to compute statistics
-            if isinstance(ds, Dataset):
-                if cfg.stats_sample_size is not None and cfg.stats_sample_size < len(ds):
-                    stats_ds = ds.shuffle(seed=0).select(range(cfg.stats_sample_size))
-                else:
-                    stats_ds = ds
-            else:
-                if cfg.stats_sample_size is not None:
-                    stats_iterable_ds = ds.shuffle(seed=0).take(cfg.stats_sample_size)
-                    stats_ds = assert_type(Dataset, Dataset.from_generator(lambda: iter(stats_iterable_ds)))
-                else:
-                    stats_ds = assert_type(Dataset, Dataset.from_generator(lambda: iter(ds)))
-
-            normalizers = fit_normalizers(
-                model,
-                stats_ds,
-                batches=allocate_batches(stats_ds["length"], cfg.token_batch_size),
-                kind=cfg.normalizer,
-                target_modules=target_modules,
-            )
-        else:
-            normalizers = {}
+        normalizers = {}
 
         processor = GradientProcessor(
             normalizers,
