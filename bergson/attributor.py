a386b45fd413e7873a8c6d82eb828b46b60cc1c8
Gonçalo Paulo <goncalo@eleuther.ai>
2025-08-06 10:00:47 +0000

    faiss optional

--- bergson/attributor.py
+++ bergson/attributor.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from time import time
 from typing import Generator, Protocol
 
-import faiss
+#import faiss
 import numpy as np
 import torch
 from numpy.lib.recfunctions import structured_to_unstructured
