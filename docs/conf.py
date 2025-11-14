e7fe43f7b6dae4f31286d29dffc715804fe93bed
Lucia Quirke <luciarosequirke@gmail.com>
2025-11-14 04:19:54 +0000

    Build docs from docstrings

---
+++ docs/conf.py
@@ -0,0 +1,6 @@
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # For Google/NumPy style docstrings
+    "sphinx.ext.viewcode",  # Adds links to source code
+    "sphinx_autodoc_typehints",  # Optional: better type hints
+]
